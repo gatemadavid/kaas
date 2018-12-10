eddcbe5e2d6d5008dd20d9852b4fea5fa3487768
amos wachira <amoswachira50@gmail.com>
2018-12-10 12:42:48 +0300

    first pull

--- users.py
+++ users.py
@@ -2,7 +2,9 @@ class User():
     """
     This is a class that defines the base user methods
     """
+
     def __init__(self, username, lastLoggedIn, isLoggedIn=False):
         self.isLoggedIn = isLoggedIn
         self.username = username
         self.lastLoggedInAt = lastLoggedIn
+
