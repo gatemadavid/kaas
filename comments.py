cbf8be2c68f09c1e778e1a2b2807c6cf36f78652
valerie <ronovalerie@gmail.com>
2018-12-10 13:48:04 +0300

    remove edit function

--- comments.py
+++ comments.py
@@ -25,15 +25,6 @@ class Comment(object):
         self.author = author
         self.timestamp = datetime.datetime.now()
 
-    def editComment(self, author, comment_id, message):
-        author = self.author
-        if loggedinUser == author:
-            for comment in comments:
-                if comment['id'] == comment_id:
-                    comment['message'] = message
-        else:
-            return {"Message": "You cannot edit this comment. You are not the owner."}
-
 
 class Reply():
     """
