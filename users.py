b2c5db09332b64e58c9b7875b1a7f5216769c4f5
ogiste <alfred.opon@strathmore.edu>
2018-12-10 12:48:18 +0300

    [Feature #16238966] User should be able to log in

--- users.py
+++ users.py
@@ -1,3 +1,5 @@
+import datetime
+
 class User():
     """
     This is a class that defines the base user methods
@@ -8,6 +10,19 @@ class User():
         self.username = username
         self.lastLoggedInAt = lastLoggedIn
 
+    def logIn(self):
+        """
+        Method signs in a user by setting loggedIn property to true and
+        updating user lastLoggedInAt timestamp
+
+        Returns
+        -------
+        None
+        """
+
+        self.isLoggedIn = True
+        self.lastLoggedInAt = datetime.datetime.today().strftime('%Y/%m/%d - %I:%M%p')
+        return None
 
 class Moderator(User):
     def delete_comment(self):
