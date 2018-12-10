3a441058e8fbdb20758dd7f7ecf6bac7e94074fa
Darius Njihia <njihiadarius@gmail.com>
2018-12-10 12:20:13 +0300

    [chore #162538784] setup user class constructor

--- users.py
+++ users.py
@@ -1,3 +1,8 @@
-"""
-Users file
-"""
+class User():
+    """
+    This is a class that defines the base user methods
+    """
+    def __init__(self, username, lastLoggedIn, isLoggedIn=False):
+        self.isLoggedIn = isLoggedIn
+        self.username = username
+        self.lastLoggedInAt = lastLoggedIn
